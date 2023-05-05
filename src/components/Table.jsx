0f58fd19a886207573cd17cd6b536ccada301a90
Vicente Viera <plantedcoot706@gmail.com>
2023-05-05 15:02:07 -0600

    First Table Completed

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -4,124 +4,124 @@ import { Card, Typography } from "@material-tailwind/react";
 import { IconButton, Menu, MenuHandler, MenuItem, MenuList } from "@material-tailwind/react";
 import { FiChevronDown } from "react-icons/fi";
 import { useNavigate } from "react-router-dom";
+import Loader from "./loaders/Loader";
 
-const TABLE_HEAD = ["ID Usuario", "E-Mail", "Nombre", "Apellido Paterno", "CURP", "RFC", "ARCO"];
-const TABLE_ROWS = [
-    {
-        name: "John Michael",
-        job: "Manager",
-        date: "23/04/18",
-    },
-    {
-        name: "Alexa Liras",
-        job: "Developer",
-        date: "23/04/18",
-    },
-    {
-        name: "Laurent Perrier",
-        job: "Executive",
-        date: "19/09/17",
-    },
-    {
-        name: "Michael Levi",
-        job: "Developer",
-        date: "24/12/08",
-    },
-    {
-        name: "Richard Gran",
-        job: "Manager",
-        date: "04/10/21",
-    },
-];
+const TABLE_HEAD = ["ID Usuario", "E-Mail", "Nombre", "Apellido Paterno", "CURP", "Celular", "Nacionalidad", "Estado", "Género", "ARCO"];
+const TABLE_ROWS = 0
  
-const Table = ({datas}) => {
-    // Este es un hook que regresa dos valores, una variable y una función setter para cambiar
-    // el valor de la variable. En cuanto el valor de la variable cambia el componente se va a
-    // reenderizar.
-    const [data, setData] = useState(datas);
+const Table = ({data}) => {
     // Este hook es de react-router. Sirve para cambiar la dirección de la barra de navegación.
     // Tiene la sintaxis de: navigate("/Dirección-a-la-que-se-quiere-navegar");
     const navigate = useNavigate();
-    // 
     const {setActive} = useContext(BackContext);
 
+    const getGender = (gender) => {
+        const map = new Map([
+            [0, "Mujer"],
+            [1, "Hombre"],
+            [2, "Otro"]
+        ]);
+
+        return map.get(gender);
+    }
+
     return (
-        <Card className="overflow-scroll h-full w-full mt-24">
-        <table className="w-full min-w-max table-auto text-left">
-            <thead>
-            <tr>
-                {TABLE_HEAD.map((head) => (
-                <th key={head} className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
-                    <Typography
-                        variant="small"
-                        color="blue-gray"
-                        className="font-normal leading-none opacity-70"
-                    >
-                    {head}
-                    </Typography>
-                </th>
-                ))}
-            </tr>
-            </thead>
-            <tbody>
+        <>
+            {data? (
+            <Card className="overflow-scroll h-full w-full mt-24">
+            <table className="w-full min-w-max table-auto text-left">
+                <thead>
+                    <tr>
+                        {TABLE_HEAD.map((head) => (
+                        <th key={head} className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
+                            <Typography
+                                variant="small"
+                                color="blue-gray"
+                                className="font-normal leading-none opacity-70"
+                            >
+                            {head}
+                            </Typography>
+                        </th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
                 {/* Desestructurar los datos que me mande renet y asignarlos a cada campo */}
-            {TABLE_ROWS.map(({ name, job, date }, index) => {
-                const isLast = index === TABLE_ROWS.length - 1;
-                const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50";
-    
-                return (
-                <tr key={name}>
-                    <td className={classes}>
-                        <Typography variant="small" color="blue-gray" className="font-normal">
-                            ID Usuario
-                        </Typography>
-                    </td>
-                    <td className={classes}>
-                        <Typography variant="small" color="blue-gray" className="font-normal">
-                            Email
-                        </Typography>
-                    </td>
-                    <td className={classes}>
-                        <Typography variant="small" color="blue-gray" className="font-normal">
-                            Nombre
-                        </Typography>
-                    </td>
-                    <td className={classes}>
-                        <Typography variant="small" color="blue-gray" className="font-normal">
-                            Apellido Paterno
-                        </Typography>
-                    </td>
-                    <td className={classes}>
-                        <Typography variant="small" color="blue-gray" className="font-normal">
-                            CURP
-                        </Typography>
-                    </td>
-                    <td className={classes}>
-                        <Typography variant="small" color="blue-gray" className="font-normal">
-                            RFC
-                        </Typography>
-                    </td>
-                    <td className="p-4">
-                        <Menu placement="right-start">
-                            <MenuHandler>
-                                <IconButton>
-                                    <FiChevronDown/>
-                                </IconButton>
-                            </MenuHandler>
-                            <MenuList>
-                                <MenuItem onClick={() => {navigate("/access"); setActive(true)}}>Acceso</MenuItem>
-                                <MenuItem onClick={() => {navigate("/rectification"); setActive(true)}}>Rectificación</MenuItem>
-                                <MenuItem>Cancelación</MenuItem>
-                                <MenuItem>Oposición</MenuItem>
-                            </MenuList>
-                        </Menu>
-                    </td>
-                </tr>
-                );
-            })}
-            </tbody>
-        </table>
-        </Card>
+                {data.map(({name, lastName, email, cellphone, nationality, state, curp, gender, id_clients}, index) => {
+                    const isLast = index === TABLE_ROWS.length - 1;
+                    const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50";
+        
+                    return (
+                    <tr key={id_clients}>
+                        <td className={classes}>
+                            <Typography variant="small" color="blue-gray" className="font-normal">
+                                {id_clients}
+                            </Typography>
+                        </td>
+                        <td className={classes}>
+                            <Typography variant="small" color="blue-gray" className="font-normal">
+                                {email}
+                            </Typography>
+                        </td>
+                        <td className={classes}>
+                            <Typography variant="small" color="blue-gray" className="font-normal">
+                                {name}
+                            </Typography>
+                        </td>
+                        <td className={classes}>
+                            <Typography variant="small" color="blue-gray" className="font-normal">
+                               {lastName}
+                            </Typography>
+                        </td>
+                        <td className={classes}>
+                            <Typography variant="small" color="blue-gray" className="font-normal">
+                                {curp}
+                            </Typography>
+                        </td>
+                        <td className={classes}>
+                            <Typography variant="small" color="blue-gray" className="font-normal">
+                                {cellphone}
+                            </Typography>
+                        </td>
+                        <td className={classes}>
+                            <Typography variant="small" color="blue-gray" className="font-normal">
+                                {nationality}
+                            </Typography>
+                        </td>
+                        <td className={classes}>
+                            <Typography variant="small" color="blue-gray" className="font-normal">
+                                {state}
+                            </Typography>
+                        </td>
+                        <td className={classes}>
+                            <Typography variant="small" color="blue-gray" className="font-normal">
+                                {getGender(gender)}
+                            </Typography>
+                        </td>
+                        <td className="p-4">
+                            <Menu placement="right-start">
+                                <MenuHandler>
+                                    <IconButton>
+                                        <FiChevronDown/>
+                                    </IconButton>
+                                </MenuHandler>
+                                <MenuList>
+                                    <MenuItem onClick={() => {navigate("/access"); setActive(true)}}>Acceso</MenuItem>
+                                    <MenuItem onClick={() => {navigate("/rectification"); setActive(true)}}>Rectificación</MenuItem>
+                                    <MenuItem>Cancelación</MenuItem>
+                                    <MenuItem>Oposición</MenuItem>
+                                </MenuList>
+                            </Menu>
+                        </td>
+                    </tr>
+                    );
+                })}
+                </tbody>
+            </table>
+            </Card>) 
+                : 
+            (<Loader/>)}
+        </>
     );
 }
 
--- src/view/Home.jsx
+++ src/view/Home.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Table from '../components/Table';
 
 const Home = () => {
+    // Este es un hook que regresa dos valores, una variable y una función setter para cambiar
+    // el valor de la variable. En cuanto el valor de la variable cambia el componente se va a
+    // reenderizar.
+    const [data, setData] = useState(null);
+
+    console.log(data)
+
+    useEffect(() => {
+        const fetchData = async() => {
+            await fetch("https://siredak.herokuapp.com/clients").
+            then(res => res.json()).
+            then(res => setData(res.response));
+        }
+
+        fetchData();
+    }, []);
+
     return (
         <div className="w-full h-full">
-            <Table/>
+            <Table data={data}/>
         </div>
     );
 }
